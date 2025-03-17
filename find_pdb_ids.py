2af8bf3f49b2a6848977c0fa458964f5560c07f1
Keita Ichii <100912526+kichi004@users.noreply.github.com>
2025-03-17 15:02:23 -0700

    added pdb start/end and chain

--- find_pdb_ids.py
+++ find_pdb_ids.py
@@ -26,23 +26,22 @@ def identify_best_structure(uniprot_id):
     response = requests.get(ebi_url)
     if response.status_code != 200:
         # print(f"Best Structure not Found for {uniprot_id}")
-        return {"uniprot_id": uniprot_id, "pdb_id": "", "coverage": 0, "start": -1, "end": -1}
+        return {"uniprot_id": uniprot_id, "pdb_id": "____", "coverage": 0.0, "chain_id": "_", 
+                "pdb_start": -1, "pdb_end": -1, "unp_start": -1, "unp_end": -1}
     
     # grab first PDB ID, coverage, start and end positions
     pdb_data = response.json()
     pdb_id = pdb_data[uniprot_id][0]["pdb_id"]
     coverage = pdb_data[uniprot_id][0]["coverage"]
+    chain_id = pdb_data[uniprot_id][0]["chain_id"]
     start = pdb_data[uniprot_id][0]["start"]
     end = pdb_data[uniprot_id][0]["end"]
+    unp_start = pdb_data[uniprot_id][0]["unp_start"]
+    unp_end = pdb_data[uniprot_id][0]["unp_end"]
 
     time.sleep(1)
-    return {"uniprot_id": uniprot_id, "pdb_id": pdb_id, "coverage": coverage, "start": start, "end": end}
-
-def fetch_pdb_structure(pdb_id):
-    '''Fetches the 3D structure of a protein from the PDB.'''
-    pdbl = Bio.PDB.PDBList()
-    pdb_file_path = pdbl.retrieve_pdb_file(pdb_id, pdir=".", file_format="pdb")
-    return pdb_file_path
+    return {"uniprot_id": uniprot_id, "pdb_id": pdb_id, "coverage": coverage, "chain_id": chain_id,
+            "pdb_start": start, "pdb_end": end, "unp_start": unp_start, "unp_end": unp_end}
 
 def main():
     uniprot_ids_list = read_list("lrr_benchmark_proteins_list.txt")
@@ -60,9 +59,9 @@ def main():
 
     # output list as tsv
     with open("result_pdbs.tsv", "w") as f:
-        f.write("UniProt ID\tPDB ID\tCoverage\tStart\tEnd\n")
+        f.write("UniProt ID\tPDB ID\tCoverage\tChain ID\tPDB Start\tPDB End\tUniprot Start\tUniprot End\n")
         for pdb_data in pdb_data_list:
-            f.write(f"{pdb_data['uniprot_id']}\t{pdb_data['pdb_id']}\t{pdb_data['coverage']}\t{pdb_data['start']}\t{pdb_data['end']}\n")
-            
+            f.write(f"{pdb_data['uniprot_id']}\t{pdb_data['pdb_id']}\t{pdb_data['coverage']}\t{pdb_data['chain_id']}\t{pdb_data['pdb_start']}\t{pdb_data['pdb_end']}\t{pdb_data['unp_start']}\t{pdb_data['unp_end']}\n")
+
 if __name__ == "__main__":
     main()
